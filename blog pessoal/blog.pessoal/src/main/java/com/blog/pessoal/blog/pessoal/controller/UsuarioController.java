7628de018d7bd47207df93f9965838dce57272e1
Giovanna <giovannaalonso28@gmail.com>
2022-01-28 16:02:13 -0300

    Adicionando camada de segurança ao blog pessoal

---
+++ blog pessoal/blog.pessoal/src/main/java/com/blog/pessoal/blog/pessoal/controller/UsuarioController.java
@@ -0,0 +1,40 @@
+package com.blog.pessoal.blog.pessoal.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.blog.pessoal.blog.pessoal.model.Usuario;
+import com.blog.pessoal.blog.pessoal.model.UsuarioLogin;
+import com.blog.pessoal.blog.pessoal.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin (origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Autentication(@RequestBody Optional<UsuarioLogin> user){
+		
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+		.body(usuarioService.CadastrarUsuario(usuario));
+	}
+	
+}
---
+++ blog pessoal/blog.pessoal/src/main/java/com/blog/pessoal/blog/pessoal/service/UsuarioService.java
@@ -0,0 +1,49 @@
+package com.blog.pessoal.blog.pessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.blog.pessoal.blog.pessoal.model.Usuario;
+import com.blog.pessoal.blog.pessoal.model.UsuarioLogin;
+import com.blog.pessoal.blog.pessoal.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario CadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder ();
+		
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		
+		return repository.save(usuario);
+	}
+	
+	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user){
+		BCryptPasswordEncoder encoder =  new BCryptPasswordEncoder ();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if (usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				
+				String auth = user.get() + ":" + user.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+			}
+		}
+		
+		return null;
+	}
+	
+}
